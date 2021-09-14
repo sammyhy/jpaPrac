26c77d054784e983cab37539556b3859fe8d65e8
sammy <sammykkoon@gmail.com>
2021-09-14 19:44:50 +0900

    learning jpa  - 양방향 매핑(mappedby)  - 연관관계 매핑

--- jpaPrac/src/main/java/hellojpa/Main.java
+++ jpaPrac/src/main/java/hellojpa/Main.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -7,6 +9,7 @@ import javax.persistence.Persistence;
 
 import hellojpa.entity.Member;
 import hellojpa.entity.MemberType;
+import hellojpa.entity.Team;
 
 public class Main {
 	public static void main(String[] args) {
@@ -17,11 +20,32 @@ public class Main {
 		tx.begin();
 		
 		try {
+			
+			Team team = new Team();
+			team.setName("Arsenal");
+			em.persist(team);
+			
 			Member member = new Member();
-			member.setId(100L);
-			member.setName("Harry kane");
-			member.setMemberType(MemberType.ADMIN);
+			member.setName("Nicolas Pepe");
+			member.setAge(26);
+			member.setTeam(team); // 연관관계의 주인에 값 설정
+			team.getMembers().add(member);
 			em.persist(member);
+			
+			em.flush();
+			em.clear();
+			
+//			//select
+//			Member findMember = em.find(Member.class, member.getId());
+//			//연관관계가 없다
+//			Team findTeam = findMember.getTeam();
+//			
+//			findTeam.getName();
+//			
+//			List<Member> members = findTeam.getMembers();
+//			for(Member member1 : members) {
+//				System.out.println("member1 : "+ member1);
+//			}
 			
 			tx.commit();
 		} catch (Exception e) {
--- jpaPrac/src/main/java/hellojpa/entity/Member.java
+++ jpaPrac/src/main/java/hellojpa/entity/Member.java
@@ -9,55 +9,59 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
 public class Member {
 	
-	@Id@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	@Column(name ="USERNAME")
 	private String name;
 	
 	private int age;
 	
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date regDate;
-	
-	@Enumerated(EnumType.STRING)
-	private MemberType memberType;
+//	@Column(name="TEAM_ID")
+//	private Long teamId;
+
+	@ManyToOne
+	@JoinColumn(name="TEAM_ID")
+	private Team team;
 	
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	public Date getRegDate() {
-		return regDate;
+
+	public Team getTeam() {
+		return team;
 	}
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
+
+	public void setTeam(Team team) {
+		this.team = team;
 	}
-	public MemberType getMemberType() {
-		return memberType;
-	}
-	public void setMemberType(MemberType memberType) {
-		this.memberType = memberType;
-	}
-	
 	
 }
+
